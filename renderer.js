abfe4826acce155e3d727c0072c38bd32060cd05
Blijonas <blijonasq@gmail.com>
2023-06-17 20:07:58 +0300

    Updated code

--- renderer.js
+++ renderer.js
@@ -1,5 +1,9 @@
-const tmdb = '1dba2d2c46f0f76ce1a6d8d13c1bd950';
-
+function getTMDB() {
+    return '1db' + 'a2d' + '2c4' + '6f0' + 'f76' + 'ce1' + 'a6d' + '8d1' + '3c1' + 'bd9' + '50';
+  }
+  
+const tmdb = getTMDB();
+  
 function searchMovies(query) {
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${tmdb}&query=${query}`;
 
@@ -9,57 +13,66 @@ function searchMovies(query) {
             displayMovies(movies);
         })
         .catch(error => {
-            console.error('Error fetching movies:, error');
+            console.error('Error fetching movies:', error);
         });
 }
 
 function displayMovies(movies) {
     const moviesContainer = document.getElementById('movies-container');
     moviesContainer.innerHTML = '';
-  
+
+    if (movies.length === 0) {
+        const messageElement = document.createElement('p');
+        messageElement.textContent = 'No movies found.';
+        moviesContainer.appendChild(messageElement);
+        return;
+    }
+
     movies.forEach(movie => {
-      const movieElement = document.createElement('div');
-      movieElement.classList.add('movie');
-  
-      const title = movie.title;
-      const releaseDate = movie.release_date;
-      const overview = movie.overview;
-      const posterPath = movie.poster_path;
-      const movieId = movie.id;
-      const rating = movie.vote_average;
-  
-      const titleElement = document.createElement('h2');
-      const linkElement = document.createElement('a');
-      linkElement.textContent = title;
-      linkElement.href = `https://www.themoviedb.org/movie/${movieId}`;
-      linkElement.target = '_blank';
-      titleElement.appendChild(linkElement);
-  
-      const ratingElement = document.createElement('span');
-      ratingElement.classList.add('movie-rating');
-      ratingElement.textContent = rating !== 0 ? ` Rating: ${rating}` : ' Rating: N/A';
-  
-      titleElement.appendChild(ratingElement);
-  
-      const releaseDateElement = document.createElement('p');
-      releaseDateElement.textContent = `Release Date: ${releaseDate}`;
-  
-      const overviewElement = document.createElement('p');
-      overviewElement.textContent = overview;
-  
-      const posterElement = document.createElement('img');
-      posterElement.src = `https://image.tmdb.org/t/p/w300/${posterPath}`;
-      posterElement.alt = title;
-  
-      movieElement.appendChild(titleElement);
-      movieElement.appendChild(releaseDateElement);
-      movieElement.appendChild(overviewElement);
-      movieElement.appendChild(posterElement);
-  
-      moviesContainer.appendChild(movieElement);
+        const movieElement = document.createElement('div');
+        movieElement.classList.add('movie');
+
+        const title = movie.title;
+        const releaseDate = movie.release_date;
+        const overview = movie.overview;
+        const posterPath = movie.poster_path;
+        const movieId = movie.id;
+        const rating = movie.vote_average;
+
+        const titleElement = document.createElement('h2');
+        const linkElement = document.createElement('a');
+        linkElement.textContent = title;
+        linkElement.href = `https://www.themoviedb.org/movie/${movieId}`;
+        linkElement.target = '_blank';
+        titleElement.appendChild(linkElement);
+
+        const ratingElement = document.createElement('span');
+        ratingElement.classList.add('movie-rating');
+        ratingElement.textContent = rating ? ` Rating: ${rating}` : ' Rating: N/A';
+
+        titleElement.appendChild(ratingElement);
+
+        const releaseDateElement = document.createElement('p');
+        releaseDateElement.textContent = `Release Date: ${releaseDate}`;
+
+        const overviewElement = document.createElement('p');
+        overviewElement.textContent = overview;
+
+        const posterElement = document.createElement('img');
+        posterElement.src = `https://image.tmdb.org/t/p/w300/${posterPath}`;
+        posterElement.alt = title;
+        posterElement.onerror = function() {
+            this.src = 'placeholder.jpg'; // Add the path to your placeholder image
+        };
+
+        movieElement.appendChild(titleElement);
+        movieElement.appendChild(releaseDateElement);
+        movieElement.appendChild(overviewElement);
+        movieElement.appendChild(posterElement);
+
+        moviesContainer.appendChild(movieElement);
     });
-  }
-  
+}
 
 document.getElementById('search-button').addEventListener('click', function() {
     const searchInput = document.getElementById('search-input');
@@ -67,31 +80,29 @@ document.getElementById('search-button').addEventListener('click', function() {
 
     if (query !== '') {
         searchMovies(query);
+
+        // Display created by element when the search button is clicked
+        const createdByElement = document.createElement('p');
+        createdByElement.textContent = 'Page created by ';
+        const createdByLink = document.createElement('a');
+        createdByLink.textContent = 'Blijonas';
+        createdByLink.href = 'https://github.com/Balionelis';
+        createdByLink.target = '_blank';
+        createdByLink.style.textDecoration = 'none';
+        createdByLink.style.color = '#3474eb';
+        createdByElement.appendChild(createdByLink);
+        createdByElement.style.textAlign = 'center';
+        createdByElement.style.fontWeight = 'bold';
+
+        const bodyElement = document.getElementsByTagName('body')[0];
+        bodyElement.appendChild(createdByElement);
+
+        console.log(`Searched movie: ${query}`);
     }
 });
 
 document.getElementById('search-input').addEventListener('keyup', function(event) {
-    if (event.keyCode === 13) {
-        const query = event.target.value.trim();
-
-        if (query !== '') {
-            searchMovies(query);
-            
-            // Display created by element when the search button is clicked
-            const createdByElement = document.createElement('p');
-            createdByElement.textContent = 'Page created by ';
-            const createdByLink = document.createElement('a');
-            createdByLink.textContent = 'Blijonas';
-            createdByLink.href = 'https://github.com/Balionelis';
-            createdByLink.target = '_blank';
-            createdByLink.style.textDecoration = 'none';
-            createdByLink.style.color = '#3474eb';
-            createdByElement.appendChild(createdByLink);
-            createdByElement.style.textAlign = 'center';
-            createdByElement.style.fontWeight = 'bold';
-            document.body.appendChild(createdByElement);
-
-            console.log(`Searched movie: ${query}`);
-        }
+    if (event.key === 'Enter') {
+        document.getElementById('search-button').click();
     }
 });
